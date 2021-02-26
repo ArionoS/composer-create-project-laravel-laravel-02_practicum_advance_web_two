c891ccd0fa76a6570396e234bde23b6e0f164e5a
ArionoS <arionoseptian0802@gmail.com>
2021-02-26 12:42:45 +0700

    Practicum 2_no 3

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "Ariono Septian Jaya / 1941720092";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index']);
+
+Route::get('/about',[AboutController::class,'about']);
+
+Route::get('/articles/{id}',[ArticleController::class,'articles']);
