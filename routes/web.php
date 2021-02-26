9047744d29c547f2ce7b20c99be00098c642755c
ArionoS <arionoseptian0802@gmail.com>
2021-02-26 13:27:21 +0700

    Practicum 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\ContactController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,58 @@ use App\Http\Controllers\PageController;
 |
 */
 
-Route::get('/',[PageController::class,'index']);
+//router biasa
+Route::get('/', function () {
+    return redirect('https://www.educastudio.com');
+ 
+});
 
-Route::get('/about',[PageController::class,'about']);
+//router prefix
+Route::prefix('/product')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        return redirect('https://www.educastudio.com/category/marbel-edu-games');
+    });
+    Route::get('/marbel-and-friends-kids-games', function () {
+        return redirect('https://www.educastudio.com/category/marbel-and-friends-kids-games');
+    });
+    Route::get('/riri-story-books', function () {
+        return redirect('https://www.educastudio.com/category/riri-story-books');
+    });
+    Route::get('/kolak-kids-songs', function () {
+        return redirect('https://www.educastudio.com/category/kolak-kids-songs');
+    });
+   });
 
-Route::get('/articles/{id}',[PageController::class,'articles']);
+//router parameter
+Route::get('/news', function () {
+    return redirect('https://www.educastudio.com/news');
+ 
+});
+
+Route::get('/news/{id}', function ($id) {
+    return 'https://www.educastudio.com/news/educa-studio-berbagi-untuk-warga-sekitar-terdampak-covid-19 '.$id;
+   });
+
+//router prefix
+Route::prefix('/program')->group(function () {
+    Route::get('/karir', function () {
+        return redirect('https://www.educastudio.com/program/karir');
+    });
+    Route::get('/magang', function () {
+        return redirect('https://www.educastudio.com/program/magang');
+    });
+    Route::get('/kunjungan-industri', function () {
+        return redirect('https://www.educastudio.com/program/kunjungan-industri');
+    });
+});    
+
+//router biasa
+Route::get('/about-us', function () {
+    return redirect('https://www.educastudio.com/about-us');
+ 
+});
+
+//route resource only
+Route::resource('/contact-us', ContactController::class)->only([
+    'index'
+   ]);
